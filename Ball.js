f68c5034dc48539a2479416ee31f54f5655e972c
Pratham1607 <78417410+Pratham1607@users.noreply.github.com>
2023-08-10 10:43:35 +0530

    Ball.js

--- Ball.js
+++ Ball.js
@@ -7,38 +7,29 @@ export default class Ball {
         this.reset();
     }
 
-
-    // this line of code gets the computed styles of the ballElem element, extracts the value of the custom CSS property --x, and converts it to a floating-point number.
-    // Setting x position
     get x() {
         return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"));
     }
-
     set x(value) {
         this.ballElem.style.setProperty("--x", value);
     }
 
-    // Setting y position
     get y() {
         return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"));
     }
-
     set y(value) {
         this.ballElem.style.setProperty("--y", value);
     }
 
     rect() {
-        // getBoundingClientRect() is a built-in method provided by the DOM (Document Object Model) API in web browsers. It's used to retrieve a DOMRect object that represents the current position and dimensions of an HTML element relative to the viewport. This method is commonly used for various tasks, such as collision detection, element positioning, and responsive design.
         return this.ballElem.getBoundingClientRect();
     }
 
-    // Creating Reset function
     reset() {
         this.x = 50;
         this.y = 50;
         this.direction = { x: 0 };
         
-        // The following while loop ensures that the initial direction of the ball is not too steep
         while (Math.abs(this.direction.x) <= .2 || Math.abs(this.direction.x) >= 0.9) {
             const heading = randomNumberBetween(0, 2 * Math.PI);
             this.direction = { x: Math.cos(heading), y: Math.sin(heading) };
@@ -53,12 +44,10 @@ export default class Ball {
         const rect = this.rect();
 
         if (rect.bottom >= window.innerHeight || rect.top <= 0) {
-            // Reverse the vertical direction of the ball
             this.direction.y *= -1;
         }
 
         if (paddleRects.some(r => isCollision(r, rect))) {
-            // Reverse the horizontal direction of the ball
             this.direction.x *= -1;
         }
     }
@@ -68,7 +57,6 @@ function randomNumberBetween(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-// Will check for collisions
 function isCollision(rect1, rect2) {
     return (
         rect1.left <= rect2.right && rect1.right >= rect2.left && rect1.top <= rect2.bottom && rect1.bottom >= rect2.top
